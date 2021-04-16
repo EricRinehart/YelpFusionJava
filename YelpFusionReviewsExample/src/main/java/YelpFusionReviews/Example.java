9954b87706385a9864173ff32bd425891c8864ca
Eric Rinehart <53406588+EricRinehart@users.noreply.github.com>
2021-04-16 00:57:58 -0400

    Add files via upload

---
+++ YelpFusionReviewsExample/src/main/java/YelpFusionReviews/Example.java
@@ -0,0 +1,120 @@
+package YelpFusionReviews;
+
+/*
+ An API endpoint in Java and Spring Boot
+ This uses key authentication to access Yelp Fusion API
+ 2 requests are made to access Yelp's data
+ The 1st request retrieves a business ID and business data to display later
+ The ID is used to find the referenced business' reviews
+ The 2nd request retrieves review data including the rating, text, and some user information
+ Ultimately, the review data and the business data are displayed
+ */
+
+import java.io.IOException;
+import okhttp3.*;
+import okhttp3.Request.Builder;
+import org.json.*;
+
+public class Example {
+
+    public static void main(String[] args)throws JSONException, IOException {
+
+        // unique API key from Yelp
+        final String YOUR_API_KEY = "xfJjPbDeriW2TcJcQeiEgh0rdBWqchWEtEb1IkKMZpLKWPEm8xBWghBSpX13MG1pMmohAk7nPPFUd5rckHo_Y4rLIcM05NZvduzX4lyD-HUbPP60osQmtdZhfl13YHYx";
+
+        // indexes for traversing JSON String body
+        final int INDEX_ID = 0;
+        final int INDEX_REVIEW = 0; // review only returns 3 reviews so values 3 and on will fail
+
+        // GET /businesses/search parameters
+        final String TERM = "pasta"; // Yelp requires a String term which can be food like coffee or even a name like Starbucks
+        final String SEARCH_LOCATION = "Durham, NC"; // Yelp requires a location <city, state>
+        String id = "";
+        String venue = "";
+        JSONObject venueLocation = null;
+        JSONArray address = null;
+        String addressFormatted = "";
+        JSONObject user = null;
+
+        // client to send requests from
+        OkHttpClient client = new OkHttpClient();
+
+        /*
+         1st request
+         YelpAPI uses businesses/search to determine unique business ID
+         business ID will be used to pull reviews
+         */
+        Request requestID = new Builder()
+                .url("https://api.yelp.com/v3/businesses/search?term=" + TERM + "&location=" + SEARCH_LOCATION + "")
+                .get()
+                .addHeader("authorization", "Bearer"+" "+ YOUR_API_KEY)
+                .build();
+
+        try {
+            /*
+             Get business ID to get review information
+             Get business information to display
+             Information to display includes name of venue and its location as an address
+             Location is an array and needs to the brackets removed
+             */
+            Response responseID = client.newCall(requestID).execute();
+
+            JSONObject jsonObject = new JSONObject(responseID.body().string().trim()); // parser
+            JSONArray myResponseID = (JSONArray)jsonObject.get("businesses");
+            id = myResponseID.getJSONObject(INDEX_ID).getString("id");
+            venue = myResponseID.getJSONObject(INDEX_ID).getString("name");
+            venueLocation = myResponseID.getJSONObject(INDEX_ID).getJSONObject("location");
+            address = (JSONArray)venueLocation.get("display_address");
+            String temp = address.toString();
+            addressFormatted = temp.replace("[", "").replace("]", "");
+
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        /*
+         2nd request
+         use business Id to pull reviews
+         According to Yelp API documentation only 3 reviews are pulled per request
+         */
+        Request requestReviews = new Builder()
+                .url("https://api.yelp.com/v3/businesses/"+id+"/reviews")
+                .get()
+                .addHeader("authorization", "Bearer"+" "+ YOUR_API_KEY)
+                .build();
+
+        try {
+            /*
+             Get user information to display from review
+             User information to display includes name and image_url
+             Get rating and text from review to display
+             Display business info collected from 1st response
+             */
+            Response responseReviews = client.newCall(requestReviews).execute();
+
+            JSONObject jsonObject = new JSONObject(responseReviews.body().string().trim()); // parser
+            JSONArray myResponseReviews = (JSONArray)jsonObject.get("reviews");
+            user = myResponseReviews.getJSONObject(INDEX_REVIEW).getJSONObject("user");
+            String name = (String)user.get("name");
+            String imageUrl = (String)user.get("image_url");
+            System.out.println(name);
+            System.out.println(imageUrl);
+            System.out.println(venue);
+            System.out.println(addressFormatted);
+            System.out.println(myResponseReviews.getJSONObject(INDEX_REVIEW).getInt("rating"));
+            System.out.println(myResponseReviews.getJSONObject(INDEX_REVIEW).getString("text"));
+
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
